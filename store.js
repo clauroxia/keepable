de7d42608a233c9085215efd764740038ca2f978
Claudia Berríos <cberriosledesma@gmail.com>
2025-10-02 11:08:42 -0500

    Display a message when there is no note

--- store.js
+++ store.js
@@ -1,43 +1,39 @@
-// Data store
-
-const initialNotes = [
-  {
-    title: "This is the title",
-    body: "This is the body for the note",
-    color: "#FFFFFF",
-    deleted: false,
-  },
-  {
-    title: "This is the title",
-    body: "This is the body for the note",
-    color: "#FBBC04",
-    deleted: false,
-  },
-  {
-    title: "This is the title",
-    body: "This is the body for the note",
-    color: "#FFF475",
-    deleted: true,
-  },
-];
-
 // Get notes from storage or setup new ones
-const notesFromStorage = JSON.parse(localStorage.getItem("notes"));
-const notes = notesFromStorage || initialNotes;
+const notes = JSON.parse(localStorage.getItem("notes")) || [];
+renderNotes(notes);
 
-//Create note
+// Create note
 function createNote(note) {
-  notes.push(note);
-  localStorage.setItem("notes", JSON.stringify(notes));
-};
+	notes.push(note);
+	localStorage.setItem("notes", JSON.stringify(notes));
+}
 
 // Delete note
 function deleteNote(note) {
-  if (isTrash(note)) {
-    const index = notes.indexOf(note);
-    notes.splice(index, 1);
-  } else {
-    note.deleted = true;
-  };
-  localStorage.setItem("notes", JSON.stringify(notes));
-};
+	if (isTrash(note)) {
+		const index = notes.indexOf(note);
+		notes.splice(index, 1);
+	} else {
+		note.deleted = true;
+	}
+	localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+// Render notes
+function renderNotes(notes) {
+	const notesList = document.querySelector(".js-notes");
+	notesList.innerHTML = "";
+	let deletedNotes = notes.filter((note) => note.deleted);
+
+	if (notes.length === 0 || deletedNotes.length === notes.length) {
+		notesList.innerHTML = `<p class="content-lg white m-auto mt-40">No notes to keep</p>`;
+		return;
+	}
+
+	notes.forEach((note) => {
+		if (!isTrash(note)) {
+			const noteEl = createNoteEl(note);
+			notesList.append(noteEl);
+		}
+	});
+}
