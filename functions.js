683fa708800818302afdde6ef40ba0bf636ec4de
Claudia Berríos <cberriosledesma@gmail.com>
2025-10-10 17:06:03 -0500

    Rename the store.js file to functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,24 @@
 // Get notes from storage or setup new ones
 const notes = JSON.parse(localStorage.getItem("notes")) || [];
 
-// Create note
-function createNote(note) {
-	notes.push(note);
-	localStorage.setItem("notes", JSON.stringify(notes));
+// Render notes
+function renderNotes(notes) {
+	const notesList = document.querySelector(".js-active-notes");
+	notesList.innerHTML = "";
+	const deletedNotes = notes.filter((note) => note.deleted);
+
+	if (notes.length === 0 || deletedNotes.length === notes.length) {
+		notesList.innerHTML = `<p class="content-lg white m-auto mt-40">No notes to keep</p>`;
+		return;
+	}
+
+	notes.forEach((note, index) => {
+		if (!isTrash(note)) {
+			const noteEl = createNoteEl(note, "notes");
+			noteEl.dataset.index = index;
+			notesList.append(noteEl);
+		}
+	});
 }
 
 // Delete note
@@ -16,24 +30,4 @@ function deleteNote(note) {
 		note.deleted = true;
 	}
 	localStorage.setItem("notes", JSON.stringify(notes));
-}
-
-// Render notes
-function renderNotes(notes) {
-	const notesList = document.querySelector(".js-active-notes");
-	notesList.innerHTML = "";
-	let deletedNotes = notes.filter((note) => note.deleted);
-
-	if (notes.length === 0 || deletedNotes.length === notes.length) {
-		notesList.innerHTML = `<p class="content-lg white m-auto mt-40">No notes to keep</p>`;
-		return;
-	}
-
-	notes.forEach((note, index) => {
-		if (!isTrash(note)) {
-			const noteEl = createNoteEl(note);
-			noteEl.dataset.index = index;
-			notesList.append(noteEl);
-		}
-	});
 }
